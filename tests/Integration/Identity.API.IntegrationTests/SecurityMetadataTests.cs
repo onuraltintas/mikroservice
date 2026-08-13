a5cb475aa75154a82655b117e5b4f01d08d439e2
Onur ALTINTAŞ <48012977+onuraltintas@users.noreply.github.com>
2026-08-13 22:16:57 +0300

    test: add authorization metadata regressions

---
+++ tests/Integration/Identity.API.IntegrationTests/SecurityMetadataTests.cs
@@ -0,0 +1,55 @@
+using FluentAssertions;
+using Microsoft.AspNetCore.Authorization;
+using Coaching.API.Controllers;
+using Identity.API.Controllers;
+using Notification.API.Controllers;
+using Xunit;
+
+namespace Identity.API.IntegrationTests;
+
+public class SecurityMetadataTests
+{
+    [Theory]
+    [InlineData(typeof(AssignmentsController))]
+    [InlineData(typeof(GoalsController))]
+    [InlineData(typeof(SessionsController))]
+    [InlineData(typeof(ExamsController))]
+    public void CoachingControllers_MustRequireAuthorization(Type controllerType)
+    {
+        controllerType
+            .GetCustomAttributes(typeof(AuthorizeAttribute), inherit: true)
+            .Should()
+            .NotBeEmpty($"{controllerType.Name} exposes user data and must not be anonymous");
+    }
+
+    [Fact]
+    public void SupportController_MustRequireAuthorization()
+    {
+        typeof(SupportController)
+            .GetCustomAttributes(typeof(AuthorizeAttribute), inherit: true)
+            .Should()
+            .NotBeEmpty("support reply is an administrative action");
+    }
+
+    [Fact]
+    public void NotificationTestEndpoint_MustRequireAuthorization()
+    {
+        var action = typeof(NotificationsController).GetMethod(nameof(NotificationsController.GetAllNotifications));
+
+        action.Should().NotBeNull();
+        action!
+            .GetCustomAttributes(typeof(AuthorizeAttribute), inherit: true)
+            .Should()
+            .NotBeEmpty("the test endpoint can return every user's notifications");
+    }
+
+    [Fact]
+    public void InternalNotificationEndpoint_MustNotAllowAnonymousAccess()
+    {
+        var action = typeof(InternalNotificationController).GetMethod(nameof(InternalNotificationController.ForwardSupport));
+
+        action.Should().NotBeNull();
+        action!.GetCustomAttributes(typeof(AllowAnonymousAttribute), inherit: true).Should().BeEmpty();
+        action.GetCustomAttributes(typeof(AuthorizeAttribute), inherit: true).Should().NotBeEmpty();
+    }
+}
