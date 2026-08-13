cb6ed46370405f774f9dc067a67f99a9fc291063
Onur ALTINTAŞ <48012977+onuraltintas@users.noreply.github.com>
2026-08-13 22:31:52 +0300

    test: reject tokenless public email confirmation

---
+++ tests/Integration/Identity.API.IntegrationTests/AuthEndpointSecurityTests.cs
@@ -0,0 +1,30 @@
+using Identity.API.Controllers;
+using Identity.Application.Commands.ConfirmEmail;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using Xunit;
+
+namespace Identity.API.IntegrationTests;
+
+public class AuthEndpointSecurityTests
+{
+    [Fact]
+    public async Task ConfirmEmail_WithoutToken_ShouldBeRejectedBeforeHandlerExecution()
+    {
+        var mediator = DispatchProxy.Create<IMediator, ThrowingMediatorProxy>();
+        var controller = new AuthController(mediator);
+
+        var result = await controller.ConfirmEmail(new ConfirmEmailCommand(Guid.NewGuid()));
+
+        Assert.IsType<BadRequestObjectResult>(result);
+    }
+
+    private class ThrowingMediatorProxy : DispatchProxy
+    {
+        protected override object? Invoke(MethodInfo? targetMethod, object?[]? args)
+        {
+            throw new InvalidOperationException("The mediator should not be called for a missing confirmation token.");
+        }
+    }
+}
