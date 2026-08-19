0c750a8420d929dc403c554eb2b7331639cfa21d
Onur ALTINTAŞ <48012977+onuraltintas@users.noreply.github.com>
2026-08-19 19:16:41 +0300

    test: require protected audit read endpoints

---
+++ tests/Integration/Identity.API.IntegrationTests/AdminAuditReadSecurityTests.cs
@@ -0,0 +1,51 @@
+using EduPlatform.Shared.Contracts.Authorization;
+using EduPlatform.Shared.Security.Authorization;
+using FluentAssertions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.API.IntegrationTests;
+
+public sealed class AdminAuditReadSecurityTests
+{
+    public static TheoryData<System.Reflection.Assembly, string, string> Controllers => new()
+    {
+        {
+            typeof(Identity.API.Controllers.AuthController).Assembly,
+            "Identity.API.Controllers.AdminAuditController",
+            "api/admin-audit/identity"
+        },
+        {
+            typeof(Notification.API.Controllers.NotificationsController).Assembly,
+            "Notification.API.Controllers.AdminAuditController",
+            "api/admin-audit/notification"
+        },
+        {
+            typeof(Coaching.API.Controllers.AssignmentsController).Assembly,
+            "Coaching.API.Controllers.AdminAuditController",
+            "api/admin-audit/coaching"
+        }
+    };
+
+    [Theory]
+    [MemberData(nameof(Controllers))]
+    public void AuditReadController_ShouldRequireSystemAdminAndOperationsPermission(
+        System.Reflection.Assembly assembly,
+        string typeName,
+        string expectedRoute)
+    {
+        var controller = assembly.GetType(typeName);
+
+        controller.Should().NotBeNull();
+        controller!.GetCustomAttributes(typeof(RouteAttribute), inherit: true)
+            .Cast<RouteAttribute>()
+            .Should().ContainSingle(attribute => attribute.Template == expectedRoute);
+        controller.GetCustomAttributes(typeof(AuthorizeAttribute), inherit: true)
+            .Cast<AuthorizeAttribute>()
+            .Should().Contain(attribute => attribute.Roles == "SystemAdmin");
+        controller.GetCustomAttributes(typeof(HasPermissionAttribute), inherit: true)
+            .Cast<HasPermissionAttribute>()
+            .Should().Contain(attribute =>
+                attribute.Policy == PlatformPermissions.Operations.View);
+    }
+}
