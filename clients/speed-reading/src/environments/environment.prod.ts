720b182292cf3b806234098034dd2aa727981ac3
Onur ALTINTAŞ <48012977+onuraltintas@users.noreply.github.com>
2026-08-26 23:58:55 +0300

    fix(speed-reading): use Eduivme Google OAuth client

--- clients/speed-reading/src/environments/environment.prod.ts
+++ clients/speed-reading/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: true,
     apiUrl: '/api',
     speedReadingApiUrl: '/api/speed-reading',
-    googleClientId: '419291322983-u9dg9ajc2us1qn5bl3gqb07k7bc6ubtg.apps.googleusercontent.com',
+    googleClientId: '503453099078-s04ol7rissjlt59sc86lo3ef79igg4mj.apps.googleusercontent.com',
     vapidPublicKey: 'BDXTL-5iQzjdtDzUdtbPDU5bwYmrbmconj_j6e4M91hR1S5TpOH9m8TQOhJrdtpHKxmMOt1uugbSL1rynlVqoUI'
 };
